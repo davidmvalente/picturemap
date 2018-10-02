47df552b53da2b1af154d9b7541d388fd7d999b4
David Valente <david@craft.co>
2018-10-02 19:11:57 +0200

    repackaging

--- picturemap/__init__.py
+++ picturemap/__init__.py
@@ -1,13 +1,12 @@
-import parsemetadata
+from .parsemetadata import *
 from json import dump
 import sys
 import os, subprocess
 
-print ('Python version is', sys.version)
 
 def build(data, target):
     #Copy basemap file to target_directory
-    with open('resources/basemap.html', 'r') as inputhtml:
+    with open(os.path.dirname(__file__)+'/basemap.html', 'r') as inputhtml:
         with open(target+'/picturemap.html', 'w') as outputhtml:
             print('Writing '+target+'/picturemap.html')
             for line in inputhtml:
@@ -33,7 +32,7 @@ def get_metadata(filenames, target_directory):
         data_array = []
         for f in filenames:
             name = str(os.path.relpath(f, target_directory)) #Relative path to file
-            date, coordinates = parsemetadata.process(f)
+            date, coordinates = process(f)
             data_array.append((name,date,coordinates))
         result = {k:d for d,k in zip(zip(*data_array),['names','dates','coordinates'])}
         print(str(len(result['names']))+' images have been added to the map: ', result['names']) #Debug: to show dataset
@@ -43,19 +42,3 @@ def set_target(paths_to_files):
     target = os.path.dirname(os.path.commonprefix(paths_to_files))
     print('Setting '+str(target)+' as destination path.')
     return target
-
-
-def main():
-    #Read arguments
-    script = sys.argv[0]
-    filenames = sys.argv[1:]
-    #Pick destination path for visualization filenames
-    target_directory = set_target(filenames)
-    #Process files
-    pictures_data = get_metadata(filenames, target_directory) #Get name, date, coordinates
-    pictures_data['centre'] = set_centre(pictures_data['coordinates']) #Find centre coordinates
-    #Build the map
-    build(pictures_data, target_directory)
-    launch(target_directory+'/picturemap.html')
-
-main()
